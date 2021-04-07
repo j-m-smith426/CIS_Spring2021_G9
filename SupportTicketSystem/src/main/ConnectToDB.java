25b83f031e41743e7c1a805a729bdf78c6a7e356
j-m-smith426 <joabsmith4226@gmail.com>
2021-04-07 11:55:04 -0400

    updated connectToDB

--- SupportTicketSystem/src/main/ConnectToDB.java
+++ SupportTicketSystem/src/main/ConnectToDB.java
@@ -3,8 +3,9 @@ package main;
 import java.sql.*;
 
 public class ConnectToDB {
-
-	public ConnectToDB() {
+	Connection Conn;
+	
+	public ConnectToDB(Connection conn) {
 	try {
 		DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
 	} catch (SQLException e1) {
@@ -12,10 +13,12 @@ public class ConnectToDB {
 		e1.printStackTrace();
 	}
 	
-	String dbURL = "jdbc:sqlserver://192.168.0.96:1433;instance=MSSQLSERVER;databaseName=SupportTicketDB";
+	String dbURL = "jdbc:sqlserver://192.168.0.96:1433;databaseName=SupportTicketDB;";
+	//local
+	//	String dbURL = "jdbc:sqlserver://localhost:1433;databaseName=SupportTicketDB;integratedSecurity=true;";
 	String user = "pass";
 	String pass = "pass";
-	Connection conn = null;
+	conn = null;
 	try {
 		conn = DriverManager.getConnection(dbURL, user, pass);
 	} catch (SQLException e) {
@@ -24,6 +27,10 @@ public class ConnectToDB {
 	}
 	if (conn != null) {
 	    System.out.println("Connected");
+	    Conn = conn;
 	}
+	}
+	public Connection getConnect() {
+		return Conn;
 	}
 }
