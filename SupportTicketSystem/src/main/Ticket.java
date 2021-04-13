1683c01518362df720ce7ce86460fedfd4fe8753
JohnSorma <Johnsorma99@gmail.com>
2021-04-13 12:42:04 -0400

    Update Ticket.java
    Added SQL Functions to add Tickets to database also randomized TicketIDs from 1-10000

--- SupportTicketSystem/src/main/Ticket.java
+++ SupportTicketSystem/src/main/Ticket.java
@@ -1,6 +1,7 @@
 package main;
 import java.util.Date;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Ticket {
 	private String requesterID; 
@@ -11,17 +12,64 @@ public class Ticket {
 	private int priority;
 	private Date dueDate;
 	ArrayList<Integer> history = new ArrayList<Integer>();
-
+	private Random rand = new Random();
 	public Ticket(String requesterID, int ticketID, String title, int category, String description, int priority, Date dueDate) {
 		super();
 		this.requesterID = requesterID;
-		this.ticketID = ticketID;
 		this.title = title;
 		this.category = category;
 		this.description = description;
 		this.priority = priority;
 		this.dueDate = dueDate;
+		this.ticketID = rand.nextInt(10000) + 1;
+		while(this.ticketID == usedTicketID(this.ticketID)){
+			this.ticketID = rand.NextInt(10000) + 1;
+		}
+		
+		addTicketToDB(this.ticketID, this.title, this.requesterID, this.category, this.description, this.priority, this.dueDate);
+
 	}
+
+
+ 	public void addTicketToDB(int ticketID, String title, String requesterID, int category, String description, int priority, Date dueDate){
+	String selectSQL = "INSERT INTO Tickets Values(?,?,?,?,?,?,?);";
+	PreparedStatement insert;
+	try{
+		insert = Conn.prepareStatement(selectSQL);
+		insert.setString(1, ticketID);
+		insert.setString(2, title);
+		insert.setString(3, requesterID);
+		insert.setString(4, category);
+		insert.setString(5, description);
+		insert.setString(6, priority);
+		insert.setString(7, dueDate);
+		insert.executeQuery();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+
+	public int usedTicketID(int ticketID){
+		String selectSQL = "SELECT * FROM Tickets WHERE TicketID = ?;";
+		PreparedStatement = search = Conn.preparedStatement(selectSQL);
+		search.setString(1,ticketID);
+		search.executeQuery();
+		LarrayList<Integer> intList = new ArrayList<Integer>();
+		while(rs.next()){
+			int name = rs.getInt("TicketID");
+			intList.add(name);
+	}
+		
+		if(intList.isEmpty()){
+			return 0;
+		}else{
+			return ticketID;
+	}
+
+
+	}
+
 	public int getPriority() {
     		return priority;
 		
