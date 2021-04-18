d4dbcc57d55c9cf9f920b04953544de5cdb9ab7a
JohnSorma <Johnsorma99@gmail.com>
2021-04-18 13:13:41 -0400

    Update User.java

--- SupportTicketSystem/src/main/User.java
+++ SupportTicketSystem/src/main/User.java
@@ -42,7 +42,6 @@ public class User {
     if((gate1) && (gate2)){
     this.username = hashUserAccount(this.username);
     this.password = hashUserAccount(this.password);
-    //this.typeOfUser = hashUserAccount(this.typeOfUser);
     String selectSQL = "INSERT INTO Users Values(?,?,?,?); ";
     PreparedStatement insert;
 	try {
